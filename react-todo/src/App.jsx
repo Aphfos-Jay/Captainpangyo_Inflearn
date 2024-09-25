ce20d3caa23370fe707da653b33c8c7444164038
Jay <aphfos.jjw@gmail.com>
2024-09-25 16:40:24 +0900

    Second Commit

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+function fetchTodos() {
+  const result = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  }
+  return result;
+}
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState(fetchTodos());
+
+  const handleInput = (event) => {
+    const value = event.target.value;
+    setInputText(value);
+  };
+
+  const handleClick = () => {
+    localStorage.setItem(inputText, inputText);
+    setInputText("");
+    setTodos((curTodos) => {
+      return [...curTodos, inputText];
+    });
+  };
+
+  const handleRemove = (todo, index) => {
+    const result = todos.filter((todoItem) => {
+      if (todoItem !== todo) {
+        return true;
+      }
+    });
+    setTodos(result);
+    localStorage.removeItem(todo);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>TODO 앱 </h1>
+        <div>
+          <input type="text" value={inputText} onChange={handleInput} />
+          <button onClick={handleClick}>add</button>
+        </div>
+        <div>
+          <ul>
+            {todos.map((todo, index) => {
+              return (
+                <li key={index}>
+                  <span>{todo}</span>
+                  <button onClick={() => handleRemove(todo, index)}>
+                    remove
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
